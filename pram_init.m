ac756f1a310d6bc8a965ce5d18f39caa53218a1c
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-03-03 10:16:55 +0530

    minor change

--- pram_init.m
+++ pram_init.m
@@ -2,21 +2,23 @@
 % parameter initialization
 
 % datapaths
-pram.TrDataDir          = './_data/h2ax_tissue/';
+function pram = pram_init()
+  pram.TrDataDir          = './_data/h2ax_tissue/';
 
-% network paramters
-pram.Nx                 = 64;
-pram.ValDataRatio       = 0.05; % ratio of training data used for validation
+  % network paramters
+  pram.Nx                 = 64;
+  pram.ValDataRatio       = 0.05; % ratio of training data used for validation
 
-% hyper-parameter of the rule-based algorithm
-pram.th_prop            = 0.99;
-pram.th_gt              = 20000;% jenny's annotation are dark dots on bright bg on 16bit image
+  % hyper-parameter of the rule-based algorithm
+  pram.th_prop            = 0.99;
+  pram.th_gt              = 20000;% jenny's annotation are dark dots on bright bg on 16bit image
 
-% training parameters 
-pram.maxEpochs          = 20;
-pram.miniBatchSize      = 256;
-pram.initLearningRate   = 1;
-pram.learningRateFactor = .1;
-pram.dropPeriod         = round(pram.maxEpochs/4);
-pram.l2reg              = 0.0001;
-pram.excEnv             = 'multi-gpu';   % 'gpu', 'multi-gpu'
+  % training parameters 
+  pram.maxEpochs          = 20;
+  pram.miniBatchSize      = 256;
+  pram.initLearningRate   = 1;
+  pram.learningRateFactor = .1;
+  pram.dropPeriod         = round(pram.maxEpochs/4);
+  pram.l2reg              = 0.0001;
+  pram.excEnv             = 'multi-gpu';   % 'gpu', 'multi-gpu'
+end
