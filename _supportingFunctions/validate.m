030099eed61165224f4e9358a9314bb8e08099be
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-03-05 18:47:55 +0530

    minor change

--- _supportingFunctions/validate.m
+++ _supportingFunctions/validate.m
@@ -21,7 +21,18 @@ function validate(net_rpn,net_rcn,pram)
       fileNameStem = fileinfo.Filename(temp+1:end-4);
 
       [L_fg I_now Area_tissue_now L_now] = segmentTissueOtsu(I_now,L_now,Nx);
-
+            
+      if pram.runTissueSeg == 1      
+        [L_fg I_now Area_tissue_now L_now] = segmentTissueOtsu(I_now,L_now,Nx);% segments the tissue foreground 
+      else
+        I_now           = normalize_tissue_to_1(I_now);
+        L_fg            = ones(size(L_now))>0;
+        L_fg            = padarray(L_fg,[Nx Nx]);
+        L_now           = padarray(L_now,[Nx Nx]);
+        I_now           = padarray(I_now,[Nx Nx]);
+        Area_tissue_now = -1;
+      end
+      
       L_proposal = apply_proposal_net(net_rpn,I_now,Nx);
       L_proposal(find(L_fg==0))=0;
       [I_proposals_now centroids Y_gt_now] = genRegionProposals(L_proposal>th_prop,L_now,I_now,Nx);
