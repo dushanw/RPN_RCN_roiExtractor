159225389c2d8ec50d9979e4e28d3ad1400f311c
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-03-16 16:31:54 +0530

    mf

--- _supportingFunctions/f_augmentDataSet.m
+++ _supportingFunctions/f_augmentDataSet.m
@@ -4,7 +4,7 @@ function [X Y] = f_augmentDataSet(X,Y)
   X180  = imrotate(X,180);
   X270  = imrotate(X,270);
   
-  [temp mxYsize] = max(size(YVal))
+  [temp mxYsize] = max(size(Y));
   
   X     = cat(4      ,X,X90,X180,X270);
   Y     = cat(mxYsize,Y,  Y,   Y,   Y);  
