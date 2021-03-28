b972324225fcf048d56c1db3f2b85e5ac362944a
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-03-28 13:30:53 +0530

    fixed some repretition bugs in proposal and gt match for rpn results and gt. Moved it to a new function for clarity

---
+++ _supportingFunctions/f_match_propBW_2_gtBW.m
@@ -0,0 +1,45 @@
+% 20210328 by Dushan N Wadduwage
+% This function matches GT to Proposals based on their centroids' seperation.
+
+function [centr_proposals,...
+          prop_tpIf1_fpIf0,...
+          centroids_fn] = f_match_propBW_2_gtBW(L_prop,L_gt)
+
+  stats_proposals   = regionprops(L_prop,'Centroid');
+  stats_gt          = regionprops(L_gt  ,'Centroid');
+
+  centr_proposals   = cat(1,stats_proposals.Centroid);
+  centr_gt          = cat(1,stats_gt.Centroid);
+
+  if ~isempty(centr_gt) & ~isempty(centr_proposals)
+    Dist_mat          = sqrt((centr_proposals(:,1) - centr_gt(:,1)').^2 + ...
+                             (centr_proposals(:,2) - centr_gt(:,2)').^2);
+
+    % find TPs, 
+    [min_dist prop_ind] = min(Dist_mat,[],1);
+    isNearApropObj      = min_dist<pram.gtDistTh;
+    gt_ind_tp_withrep   = find(isNearApropObj==1);
+    prop_ind_tp_withrep = prop_ind(gt_ind_tp_withrep);
+    [prop_ind_tp, ...
+     ia,...
+     ic ]               = unique(prop_ind_tp_withrep); % [C,ia,ic] = unique(A) and C = A(ia) and A = C(ic)
+
+    % find FNs
+    gt_ind_fn           = setdiff(1:size(centr_gt,1),...
+                                  gt_ind_tp_withrep(ia));  
+    centroids_fn        = centr_gt(gt_ind_fn,:);
+
+    % compile Y_gt for RCN-classigication
+    prop_tpIf1_fpIf0              = zeros(size(centr_proposals,1),1);
+    prop_tpIf1_fpIf0(prop_ind_tp) = 1;
+
+    xx(k,i,:) = [length(gt_ind_fn)+length(prop_ind_tp) size(centr_gt,1)];
+  else
+    prop_tpIf1_fpIf0    = zeros(size(Centroids,1),1);
+    centroids_fn        = centr_gt(gt_ind_fn,:);
+  end      
+
+
+
+
+end
