7e9cfd3436c24233a7820669b4936d140463d322
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-03-28 17:14:46 +0530

    updated augmetation function to be able to augment only positives

--- _supportingFunctions/f_augmentDataSet.m
+++ _supportingFunctions/f_augmentDataSet.m
@@ -1,6 +1,14 @@
 
-function [X Y] = f_augmentDataSet(X,Y)
-  X0        = X;
+function [X Y] = f_augmentDataSet(X,Y,onlyPos)
+
+  if ~isempty(onlyPos) && onlyPos==1  
+    X0      = X(:,:,:,Y=='1');
+    Y0      = Y(Y=='1');
+  else
+    X0      = X;
+    Y0      = Y;
+  end  
+  
   X90       = imrotate(X, 90);
   X180      = imrotate(X,180);
   X270      = imrotate(X,270);
@@ -15,8 +23,12 @@ function [X Y] = f_augmentDataSet(X,Y)
   
   [temp mxYsize] = max(size(Y));
   
-  X         = cat(4      ,X0, X90, X180, X270, X0_flp, X90_flp, X180_flp, X270_flp);
-  Y         = cat(mxYsize,Y ,   Y,    Y,    Y,      Y,       Y,        Y,        Y);  
+  X         = cat(4      ,X , X90, X180, X270, X0_flp, X90_flp, X180_flp, X270_flp);
+  Y         = cat(mxYsize,Y ,  Y0,   Y0,   Y0,     Y0,      Y0,       Y0,       Y0);
+  
+  randInds  = randperm(length(Y));
+  X         = X(:,:,:,randInds);
+  Y         = Y(randInds);
 end
 
       
